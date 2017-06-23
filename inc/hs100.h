dd1988cf77a1b0cf33f579f326b700cac2ecb136
Hank Barta <hbarta@gmail.com>
2017-06-23 15:33:33 -0500

    declare exported functions in hs100.c

---
+++ inc/hs100.h
@@ -0,0 +1,8 @@
+/** @file hs100.h functions for communicating with TP-Link devices
+ */
+#ifndef HS100_H
+#define HS100_H
+
+unsigned char *buildOutgoing(const char* msg, unsigned char *buf);
+
+#endif // HS100_H
