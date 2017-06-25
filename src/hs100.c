ea2bbda622ae26606e5a3fccf09a8fd7e951ec7b
Hank Barta <hbarta@gmail.com>
2017-06-25 15:43:36 -0500

    started coding sendMsg() and added asserts

--- src/hs100.c
+++ src/hs100.c
@@ -4,7 +4,12 @@
  */
 
 #include <string.h>
+#include <assert.h>
+// #include <sys/socket.h>
+#include <netdb.h>
+#include <stdio.h>
 #include "util.h"
+#include "hs100.h"
 
 /** @brief Take test string (JSON) and build outgoing message in buffer 
  * buf. Return the address of buf for convenience.
@@ -18,8 +23,45 @@
  */
 unsigned char *buildOutgoing(const char *msg, unsigned char *buf)
 {
+    assert(msg != NULL);
+    assert(buf != NULL);
+
     size_t inLen = strlen(msg);
     memset(buf, '\0', sizeof(unsigned char));
     encrypt(msg, buf + 4, inLen);
     return buf;
+}
+
+enum TPLINK_STATUS sendMsg(const char *host, const char *msg, char *reply, size_t relpyLen)
+{
+    assert(host != NULL);
+    assert(msg != NULL);
+    assert(reply != NULL);
+    assert(relpyLen > 0);
+
+    struct hostent *hent = gethostbyname(host);
+    (void)(hent);
+    printf("hostent %p\n", (void *)hent);
+    if (hent != NULL)
+    {
+        if (hent->h_addr == 0)
+        {
+            return TPLNK_NOIP;
+        }
+        else
+        {
+            // test code to display IP addresss returned
+            int i;
+            for (i = 0; i < hent->h_length - 1; i++)
+                printf("%u.", (unsigned int)(hent->h_addr[i] & 0xFF));
+            printf("%u\n", (unsigned int)(hent->h_addr[i] & 0xFF));
+            // end test code
+        }
+    }
+    else
+    {
+        return TPLNK_NOIP;
+    }
+
+    return TPLNK_OK;
 }
