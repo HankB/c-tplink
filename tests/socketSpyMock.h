ee6a111793336ba9e75ecf16428085cd3a9c2bf5
Hank Barta <hbarta@gmail.com>
2017-07-20 21:40:58 -0500

    declare public functions in socketSpyMock.c

---
+++ tests/socketSpyMock.h
@@ -0,0 +1,9 @@
+/** @file socketSpyMock.h declarations for some test S/W
+ */
+#ifndef SOCKETSPYMOCK_H
+#define SOCKETSPYMOCK_H
+
+bool forceSocketFail(bool force, int err);
+
+
+#endif // SOCKETSPYMOCK_H
